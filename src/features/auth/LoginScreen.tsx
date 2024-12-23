4a5664d79b384359e4647e574f4081712be580dc
devkush0999 <devesh2cse@gmail.com>
2024-12-23 23:19:10 +0530

    added login screen

---
+++ src/features/auth/LoginScreen.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const LoginSreen = () => {
+  return (
+    <View>
+      <Text>LoginSreen</Text>
+    </View>
+  );
+};
+
+export default LoginSreen
